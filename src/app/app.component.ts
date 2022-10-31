78f861a7aa835dd8809c86ba59131106edb75617
Marcos Gabriel Lopes <marcos_l@cooperflora.intranet>
2022-10-31 18:26:01 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'RSL-Front-1.1';
+}
